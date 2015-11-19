dcf45713ccbeb0129c62a5f4e41c9d1c099787db
Theo Zourzouvillys <theo@jive.com>
2015-11-18 22:08:22 -0700

    remove errenous imports

--- graphql-client/src/main/java/io/joss/graphql/client/channel/loopback/LoopbackChannel.java
+++ graphql-client/src/main/java/io/joss/graphql/client/channel/loopback/LoopbackChannel.java
@@ -2,11 +2,9 @@ package io.joss.graphql.client.channel.loopback;
 
 import java.util.concurrent.atomic.AtomicReference;
 
-import io.jgql.core.binder.execution.case1.TestCase1.MyRoot;
 import io.joss.graphql.client.runtime.GQLChannel;
 import io.joss.graphql.client.runtime.GQLExecution;
 import io.joss.graphql.client.runtime.GQLPreparedStatement;
-import io.joss.graphql.core.binder.BasicExecutor;
 import io.joss.graphql.core.binder.TypeBindingResult;
 import io.joss.graphql.core.binder.TypeScanner;
 import io.joss.graphql.core.binder.execution.QueryEnvironment;
@@ -16,7 +14,6 @@ import io.joss.graphql.core.binder.runtime.DataContext;
 import io.joss.graphql.core.binder.runtime.DataContexts;
 import io.joss.graphql.core.doc.GQLDocument;
 import io.joss.graphql.core.doc.GQLSelectedOperation;
-import io.joss.graphql.core.parser.GQLParser;
 import io.joss.graphql.core.value.GQLObjectValue;
 import io.joss.graphql.core.value.GQLValue;
 
