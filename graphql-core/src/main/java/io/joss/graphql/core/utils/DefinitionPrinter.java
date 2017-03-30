683361718724a6a9bd1d5ff7ab24540d229dfd79
Theo Zourzouvilys <theo@zrz.io>
2017-03-29 19:03:06 -0500

    print out directives

--- graphql-core/src/main/java/io/joss/graphql/core/utils/DefinitionPrinter.java
+++ graphql-core/src/main/java/io/joss/graphql/core/utils/DefinitionPrinter.java
@@ -3,6 +3,7 @@ package io.joss.graphql.core.utils;
 import java.io.PrintStream;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 import io.joss.graphql.core.doc.GQLArgument;
 import io.joss.graphql.core.doc.GQLDefinitionVisitor;
@@ -14,6 +15,7 @@ import io.joss.graphql.core.doc.GQLInlineFragmentSelection;
 import io.joss.graphql.core.doc.GQLOperationDefinition;
 import io.joss.graphql.core.doc.GQLSelectionVisitor;
 import io.joss.graphql.core.doc.GQLVariableDefinition;
+import io.joss.graphql.core.parser.SyntaxErrorException;
 import io.joss.graphql.core.value.GQLBooleanValue;
 import io.joss.graphql.core.value.GQLEnumValueRef;
 import io.joss.graphql.core.value.GQLFloatValue;
@@ -24,210 +26,206 @@ import io.joss.graphql.core.value.GQLStringValue;
 import io.joss.graphql.core.value.GQLValueVisitor;
 import io.joss.graphql.core.value.GQLVariableRef;
 
-public class DefinitionPrinter implements GQLDefinitionVisitor<Void>, GQLSelectionVisitor<Void>, GQLValueVisitor<Void>
-{
+public class DefinitionPrinter implements GQLDefinitionVisitor<Void>, GQLSelectionVisitor<Void>, GQLValueVisitor<Void> {
 
-  private PrintStream strm;
-  private int nodeId;
+	private PrintStream strm;
+	private int nodeId;
 
-  public DefinitionPrinter(PrintStream strm)
-  {
-    this.strm = strm;
-  }
+	public DefinitionPrinter(PrintStream strm) {
+		this.strm = strm;
+	}
 
-  @Override
-  public Void visitOperation(GQLOperationDefinition op)
-  {
+	@Override
+	public Void visitOperation(GQLOperationDefinition op) {
 
-    if (op.name() != null)
-    {
+		if (op.name() != null) {
 
-      switch (op.type())
-      {
-        case Mutation:
-          strm.print("mutation");
-          break;
-        case Query:
-          strm.print("query");
-          break;
-        case Subscription:
-          strm.print("subscription");
-          break;
-      }
+			switch (op.type()) {
+			case Mutation:
+				strm.print("mutation");
+				break;
+			case Query:
+				strm.print("query");
+				break;
+			case Subscription:
+				strm.print("subscription");
+				break;
+			}
 
-      if (op.name() != null)
-      {
-        strm.print(" ");
-        strm.print(op.name());
-      }
+			if (op.name() != null) {
+				strm.print(" ");
+				strm.print(op.name());
+			}
 
-    }
+		}
 
-    for (GQLVariableDefinition arg : op.vars())
-    {
-      strm.print('(');
-      strm.print(arg.name());
-      strm.print(':');
-      // arg.value().apply(this);
-      strm.print(')');
-    }
+		for (GQLVariableDefinition arg : op.vars()) {
+			strm.print('(');
+			strm.print(arg.name());
+			strm.print(':');
+			// arg.value().apply(this);
+			strm.print(')');
+		}
 
-    printDirectives(op.directives());
+		printDirectives(op.directives());
 
-    strm.print("{");
-    this.nodeId = 0;
-    op.selections().forEach(sel -> sel.apply(this));
-    strm.print("}");
+		strm.print("{ ");
+		this.nodeId = 0;
+		op.selections().forEach(sel -> sel.apply(this));
+		strm.print(" }");
 
-    return null;
-  }
+		return null;
+	}
 
-  @Override
-  public Void visitFragment(GQLFragmentDefinition frag)
-  {
-    // TODO Auto-generated method stub
-    return null;
-  }
+	@Override
+	public Void visitFragment(GQLFragmentDefinition frag) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-  //
+	//
 
-  @Override
-  public Void visitFieldSelection(GQLFieldSelection selection)
-  {
-    if (this.nodeId++ > 0)
-    {
-      strm.print(",");
-    }
-    if (selection.alias() != null)
-    {
-      strm.print(selection.alias());
-      strm.print(":");
-    }
-    strm.print(selection.name());
+	/**
+	 * Print the field selection out.
+	 */
 
-    if (!selection.args().isEmpty())
-    {
-      strm.print('(');
-      int count = 0;
-      for (GQLArgument arg : selection.args())
-      {
-        if (count++ > 0)
-        {
-          strm.print(',');
-        }
-        strm.print(arg.name());
-        strm.print(':');
-        arg.value().apply(this);
-      }
-      strm.print(')');
-    }
+	@Override
+	public Void visitFieldSelection(GQLFieldSelection selection) {
 
-    printDirectives(selection.directives());
+		if (this.nodeId++ > 0) {
+			strm.print(",");
+		}
 
-    if (!selection.selections().isEmpty())
-    {
-      int saved = this.nodeId;
-      this.nodeId = 0;
-      strm.print("{");
-      selection.selections().forEach(sel -> sel.apply(this));
-      strm.print("}");
-      // no comma is needed after braces, but we add them anyway.
-      this.nodeId = saved;
-    }
-    return null;
-  }
+		if (selection.alias() != null) {
+			strm.print(selection.alias());
+			strm.print(":");
+		}
 
-  private void printDirectives(List<GQLDirective> directives)
-  {
-    for (GQLDirective d : directives)
-    {
-      strm.print("@");
-      strm.print(d.name());
-      strm.print('(');
-      strm.print(')');
-    }
-  }
+		strm.print(selection.name());
 
-  @Override
-  public Void visitFragmentSelection(GQLFragmentSpreadSelection selection)
-  {
-    // TODO Auto-generated method stub
-    return null;
-  }
+		if (!selection.args().isEmpty()) {
+			strm.print('(');
+			int count = 0;
+			for (GQLArgument arg : selection.args()) {
+				if (count++ > 0) {
+					strm.print(',');
+				}
+				strm.print(arg.name());
+				strm.print(':');
+				arg.value().apply(this);
+			}
+			strm.print(')');
+		}
 
-  @Override
-  public Void visitInlineFragment(GQLInlineFragmentSelection selection)
-  {
-    // TODO Auto-generated method stub
-    return null;
-  }
+		if (!selection.directives().isEmpty()) {
+			strm.print(" ");
+			printDirectives(selection.directives());
+		}
 
-  //
+		if (!selection.selections().isEmpty()) {
+			int saved = this.nodeId;
+			this.nodeId = 0;
+			strm.print("{");
+			selection.selections().forEach(sel -> sel.apply(this));
+			strm.print("}");
+			// no comma is needed after braces, but we add them anyway.
+			this.nodeId = saved;
+		}
 
-  @Override
-  public Void visitVarValue(GQLVariableRef value)
-  {
-    strm.print('$');
-    strm.print(value.name());
-    return null;
-  }
+		return null;
 
-  @Override
-  public Void visitObjectValue(GQLObjectValue value)
-  {
-    // TODO Auto-generated method stub
-    return null;
-  }
+	}
 
-  @Override
-  public Void visitListValue(GQLListValue value)
-  {
-    AtomicInteger val = new AtomicInteger(0);
-    value.values().forEach(item -> {
-      if (val.incrementAndGet() > 1)
-      {
-        strm.print(',');
-      }
-      item.apply(this);
-    });
-    return null;
-  }
+	private String generateDirective(GQLDirective directive) {
 
-  @Override
-  public Void visitBooleanValue(GQLBooleanValue value)
-  {
-    strm.print(value == GQLBooleanValue.TRUE ? "true" : "false");
-    return null;
-  }
+		StringBuilder sb = new StringBuilder();
 
-  @Override
-  public Void visitIntValue(GQLIntValue value)
-  {
-    strm.print(value.value());
-    return null;
-  }
+		sb.append("@").append(directive.name());
 
-  @Override
-  public Void visitStringValue(GQLStringValue value)
-  {
-    strm.print('"');
-    strm.print(value.value().replace("\\", "\\\\").replace("\"", "\\\""));
-    strm.print('"');
-    return null;
-  }
+		if (!directive.args().isEmpty()) {
+			String x = directive.args().stream()
+					.map(arg -> String.format("%s: %s", arg.name(), arg.value()))
+					.collect(Collectors.joining(", "));
+			sb.append('(').append(x).append(')');
+		}
 
-  @Override
-  public Void visitFloatValue(GQLFloatValue value)
-  {
-    strm.print(value.value());
-    return null;
-  }
+		return sb.toString();
 
-  @Override
-  public Void visitEnumValueRef(GQLEnumValueRef value)
-  {
-    strm.print(value.value());
-    return null;
-  }
+	}
+
+	private void printDirectives(List<GQLDirective> directives) {
+		strm.print(directives.stream().map(this::generateDirective).collect(Collectors.joining(" ")));
+	}
+
+	@Override
+	public Void visitFragmentSelection(GQLFragmentSpreadSelection selection) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void visitInlineFragment(GQLInlineFragmentSelection selection) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	//
+
+	@Override
+	public Void visitVarValue(GQLVariableRef value) {
+		strm.print('$');
+		strm.print(value.name());
+		return null;
+	}
+
+	@Override
+	public Void visitObjectValue(GQLObjectValue value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void visitListValue(GQLListValue value) {
+		AtomicInteger val = new AtomicInteger(0);
+		value.values().forEach(item -> {
+			if (val.incrementAndGet() > 1) {
+				strm.print(',');
+			}
+			item.apply(this);
+		});
+		return null;
+	}
+
+	@Override
+	public Void visitBooleanValue(GQLBooleanValue value) {
+		strm.print(value == GQLBooleanValue.TRUE ? "true" : "false");
+		return null;
+	}
+
+	@Override
+	public Void visitIntValue(GQLIntValue value) {
+		strm.print(value.value());
+		return null;
+	}
+
+	@Override
+	public Void visitStringValue(GQLStringValue value) {
+		strm.print('"');
+		strm.print(value.value().replace("\\", "\\\\").replace("\"", "\\\""));
+		strm.print('"');
+		return null;
+	}
+
+	@Override
+	public Void visitFloatValue(GQLFloatValue value) {
+		strm.print(value.value());
+		return null;
+	}
+
+	@Override
+	public Void visitEnumValueRef(GQLEnumValueRef value) {
+		strm.print(value.value());
+		return null;
+	}
 
 }
--- graphql-core/src/test/java/io/joss/graphql/core/utils/DefinitionPrinterTest.java
+++ graphql-core/src/test/java/io/joss/graphql/core/utils/DefinitionPrinterTest.java
@@ -10,46 +10,49 @@ import org.junit.Test;
 import io.joss.graphql.core.doc.GQLDocument;
 import io.joss.graphql.core.parser.GQLParser;
 
-public class DefinitionPrinterTest
-{
+public class DefinitionPrinterTest {
 
-  /**
-   * make sure the parse/serialze round trip isn't lossy.
-   * 
-   * @throws IOException
-   */
+	/**
+	 * make sure the parse/serialze round trip isn't lossy.
+	 * 
+	 * @throws IOException
+	 */
 
-  @Test
-  public void test() throws IOException
-  {
+	@Test
+	public void test() throws IOException {
+		checkEquality("query { a }");
+		checkEquality("query { a }");
+		checkEquality("query { a , b }");
+		checkEquality("query { a, b { c } }");
+		checkEquality("query Moo { a, b(a: true) }");
+		checkEquality("{ a }");
+		checkEquality("{ a (text: \"in\") }");
 
-    checkEquality("query { a }");
-    checkEquality("query { a }");
-    checkEquality("query { a , b }");
-    checkEquality("query { a, b { c } }");
-    checkEquality("query Moo { a, b(a: true) }");
-    checkEquality("{ a }");
-    checkEquality("{ a (text: \"in\") }");
-    
-  }
+	}
 
-  public void checkEquality(String input) throws IOException
-  {
+	@Test
+	public void testAnnotations() throws IOException {
+		checkEquality("query { id @export(as: \"myname\") @live @bob }");
+	}
 
+	public void checkEquality(String input) throws IOException {
 
-    GQLDocument doc = GQLParser.parseDocument(input);
+		GQLDocument doc = GQLParser.parseDocument(input);
 
-    //
-    ByteArrayOutputStream os = new ByteArrayOutputStream();
-    PrintStream ps = new PrintStream(os);
-    new GQLDocumentPrinter().print(doc, ps);
-    ps.close();
-    os.flush();
-    
-    GQLDocument doc2 = GQLParser.parseDocument(os.toString());
-    
-    Assert.assertEquals(doc, doc2);
+		//
+		ByteArrayOutputStream os = new ByteArrayOutputStream();
+		PrintStream ps = new PrintStream(os);
+		new GQLDocumentPrinter().print(doc, ps);
+		ps.close();
+		os.flush();
 
-  }
+		System.err.println(os.toString());
+		
+		
+		GQLDocument doc2 = GQLParser.parseDocument(os.toString());
+
+		Assert.assertEquals(doc, doc2);
+
+	}
 
 }
