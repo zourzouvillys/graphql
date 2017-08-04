3386e4d63c737b09c95c234542c2ba588cc1be50
Theo Zourzouvilys <theo@zrz.io>
2017-08-04 14:26:18 -0500

    updates

--- graphql-schema/src/main/java/io/joss/graphql/core/schema/model/ObjectFieldParam.java
+++ graphql-schema/src/main/java/io/joss/graphql/core/schema/model/ObjectFieldParam.java
@@ -1,15 +1,21 @@
 package io.joss.graphql.core.schema.model;
 
 import io.joss.graphql.core.decl.GQLArgumentDefinition;
+import io.joss.graphql.core.value.GQLValue;
+import lombok.Getter;
 
 public class ObjectFieldParam {
 
   private final String name;
   private final TypeRef<InputCompatibleType> typeRef;
 
+  @Getter
+  private final GQLValue defaultValue;
+
   public ObjectFieldParam(TypeBuilder builder, GQLArgumentDefinition arg) {
     this.name = arg.name();
     this.typeRef = TypeRef.createInputCompatible(builder, arg.type());
+    this.defaultValue = arg.defaultValue();
   }
 
   public String getName() {
